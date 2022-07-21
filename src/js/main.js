c9b0eb53240119db5b9a4684aeae3939ddc442ac
UAMaksym <maksym.shmihel@gmail.com>
2022-07-21 19:52:54 +0300

    Header was corrected

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,26 @@
-let burger = document.querySelector('.intro_menu');
-let burger_menu = document.querySelector('.navigation')
-let intro_info = document.querySelector('.intro_info');
-let soacial_links = document.querySelector('.social_links');
-let circle = document.querySelector('.circle');
+const burger = document.querySelector('.intro_menu');
+const burgeMenu = document.querySelector('.navigation')
+const introInfo = document.querySelector('.intro_info');
+const soacialLinks = document.querySelector('.social_links');
+const circle = document.querySelector('.circle');
+const listItem = document.querySelectorAll('.list_link');
 
 
-burger.addEventListener('click', function() {
+// listItem.addEventListener('click', function() {
+
+// })
+
+for (let i = 0; i < listItem.length; i++) {
+    listItem[i].addEventListener('click', () => {
+        listItem[i].classList.toggle('active');
+    });
+}
+
+burger.addEventListener('click', function () {
     burger.classList.toggle('active');
-    burger_menu.classList.toggle('active');
-    intro_info.classList.toggle('active');
-    soacial_links.classList.toggle('active');
+    burgeMenu.classList.toggle('active');
+    introInfo.classList.toggle('active');
+    soacialLinks.classList.toggle('active');
     circle.classList.toggle('active');
 
 })
