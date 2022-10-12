c7d81463e23164da88844f89295e3ebaf9c93231
Maksym Shmihel <maksym.shmihel@gmail.com>
2022-10-12 18:52:26 +0300

    Header is almost done

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,13 @@
 const burger = document.querySelector('.intro_menu');
-const burgeMenu = document.querySelector('.navigation')
+const burgeMenu = document.querySelector('.nav_wrap')
 const introInfo = document.querySelector('.intro_info');
-const soacialLinks = document.querySelector('.social_links');
 const circle = document.querySelector('.circle');
-const listItem = document.querySelectorAll('.list_link');
+const listItems = document.querySelectorAll('.list_item');
 
-
-// listItem.addEventListener('click', function() {
-
-// })
-
-for (let i = 0; i < listItem.length; i++) {
-    listItem[i].addEventListener('click', () => {
-        listItem[i].classList.toggle('active');
+for (let i = 0; i < listItems.length; i++) {
+    listItems[i].addEventListener('click', () => {
+        listItems.forEach(item => item.classList.remove('active'));
+        listItems[i].classList.add('active');
     });
 }
 
@@ -20,7 +15,5 @@ burger.addEventListener('click', function () {
     burger.classList.toggle('active');
     burgeMenu.classList.toggle('active');
     introInfo.classList.toggle('active');
-    soacialLinks.classList.toggle('active');
     circle.classList.toggle('active');
-
 })
