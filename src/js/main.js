8a9468e5990a2224a0295ceb7d62572d58dc5ec6
Maksym Shmihel <maksym.shmihel@gmail.com>
2023-01-01 20:52:47 +0200

    PC version almost done

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
+const body = document.body;
 const burger = document.querySelector('.intro_menu');
 const burgeMenu = document.querySelector('.nav_wrap')
 const introInfo = document.querySelector('.intro_info');
 const circle = document.querySelector('.circle');
 const listItems = document.querySelectorAll('.list_item');
+const listProjectItem = document.querySelectorAll('.project_topics_list_item');
 
 for (let i = 0; i < listItems.length; i++) {
     listItems[i].addEventListener('click', () => {
@@ -11,9 +13,18 @@ for (let i = 0; i < listItems.length; i++) {
     });
 }
 
+for (let i = 0; i < listProjectItem.length; i++) {
+    listProjectItem[i].addEventListener('click', () => {
+        listProjectItem.forEach(item => item.classList.remove('active'));
+        listProjectItem[i].classList.add('active');
+    })
+}
+
 burger.addEventListener('click', function () {
+    body.classList.toggle('active');
     burger.classList.toggle('active');
     burgeMenu.classList.toggle('active');
     introInfo.classList.toggle('active');
     circle.classList.toggle('active');
 })
+
